c357a1c94ad8de47c580c49521cdc20eb6b8c547
xh1234 <xh3484@bard.edu>
2016-08-16 15:13:37 -0400

    Update quote.js

--- workshop1/quote.js
+++ workshop1/quote.js
@@ -33,9 +33,9 @@ var q = document.getElementById("quote");
 
 q.innerHTML =
     "\"The " + programmer +
-    ", like the poet, works only slightly removed from pure thought-stuff. He builds his " +
+    ", like the actress, works only slightly removed from Hollywood. She builds her " +
     castles + " in the " + air + ", from " + air + ", creating by " + exertion + " of the imagination.\"" +
-    "\u2014" +  "Fred Brooks, " + programmer;
+    "\u2014" +  "Anna Wintour, " + programmer;
 
 q.innerHTML += "<br><br><br>";
 
